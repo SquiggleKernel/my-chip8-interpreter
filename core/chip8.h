509e4202b1a8addbf210469b3b0e9fa2eace6dd3
Amrit <amritkumarmeena01@gmail.com>
2026-07-12 07:21:37 +0530

    basic structs implemented

--- core/chip8.h
+++ core/chip8.h
@@ -5,4 +5,72 @@
 #ifndef MY_CHIP8_INTERPRETER_CHIP8_H
 #define MY_CHIP8_INTERPRETER_CHIP8_H
 
+#include <array>
+#include <cstdint>
+
+struct Chip8 {
+    // 4Kib of RAM, 16 8 bit registers and 16 bit Index register
+    std::array<uint8_t, 4096> memory{};
+    std::array<uint8_t, 16> v_{};
+    uint16_t indexRegister{};
+
+    //program counter and stack pointer
+    uint16_t pc{0x200};
+    uint8_t sp{};
+
+    // 16 level stack
+    std::array<uint16_t, 16 > stack{};
+
+    // timers
+    uint8_t delayTimer{0};
+    uint8_t soundTimer{0};
+
+    // display
+    std::array<std::array<bool, 64>, 32> display{};
+
+
+    uint16_t getOpcode() {
+        return static_cast<uint16_t>( memory[pc]<<8 | memory [pc+1]);
+    }
+
+};
+
+struct Instructions {
+    std::uint16_t opcode{};
+    // Common Chip-8 components decoded from the opcode
+    uint8_t type() const { return (opcode & 0xF000) >> 12;    } // First nibble
+    uint8_t x()    const { return (opcode & 0x0F00) >> 8;     } // Second nibble or lower 4 bit of 1st byte
+    uint8_t y()    const { return (opcode & 0x00F0) >> 4;     } // Third nibble or upper 4 bit of 2nd byte
+    uint8_t n()    const { return (opcode & 0x000F);          } // Fourth nibble
+    uint8_t kk()   const { return (opcode & 0x00FF);          } // last byte of the instruction
+    uint16_t nnn() const { return (opcode & 0x0FFF);          } // Last 12 bits
+};
+
+
+struct RNG {
+    uint16_t state;
+
+    // Call this once when the emulator starts
+    void seed(uint16_t val) {
+        // State cannot be 0, or the math will always return 0
+        state = (val == 0) ? 0xACE1 : val;
+    }
+
+    // Generates the next number
+    uint8_t get_byte() {
+        state ^= state << 7;
+        state ^= state >> 9;
+        state ^= state << 8;
+        return static_cast<std::uint8_t>(state & 0xFF);
+    }
+};
+
+struct Quirks {
+    bool shiftInstruction {true};       // true on original chip-8
+    bool registerIndexIncrement{true};  // true on original chip-8
+    bool jumpWithOffset{true};          // tells what offset to use during jump instruction, V0 on original, Vx on new
+    bool flagInstructionReset{true};    // true on original chip-8
+    bool clipping{true};                // true on original chip-8
+};
+
 #endif //MY_CHIP8_INTERPRETER_CHIP8_H
